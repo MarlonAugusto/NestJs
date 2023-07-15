0485906e85111c8bd8929ec78fa3cc751ebc2ca8
MarlonAugusto <l.marlonaugusto@gmail.com>
2023-07-15 13:31:20 -0300

    [FEAT] Finalização CRUD Users + Postman Export

--- loja/src/user/user.repository.ts
+++ loja/src/user/user.repository.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from './user.entity';
 
 @Injectable()
 export class UserRepository {
-  private users = [];
+  private users: UserEntity[] = [];
 
-  async createUser(user) {
+  async createUser(user: UserEntity) {
     this.users.push(user);
   }
 
@@ -15,5 +16,35 @@ export class UserRepository {
   async MailExistent(email: string) {
     const userMail = this.users.find((user) => user.email === email);
     return userMail !== undefined;
+  }
+
+  private findById(id: string) {
+    const possibleUser = this.users.find((savedUser) => savedUser.id === id);
+
+    if (!possibleUser) {
+      throw new Error('Usuário não encontrado');
+    }
+
+    return possibleUser;
+  }
+
+  async update(id: string, newInfo: Partial<UserEntity>) {
+    const user = this.findById(id);
+    Object.entries(newInfo).forEach(([key, value]) => {
+      if (key === 'id') {
+        return;
+      }
+
+      user[key] = value;
+    });
+
+    return user;
+  }
+
+  async delete(id: string) {
+    const user = this.findById(id);
+    this.users = this.users.filter((savedUser) => savedUser.id !== id);
+
+    return user;
   }
 }
